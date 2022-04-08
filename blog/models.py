73b76653d7fb680dd956dc2ec41d37f5628a8200
Akamig <akamig@posteo.net>
2022-04-08 16:28:52 +0900

    file and media uploader added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from distutils.command import upload
 from django.db import models
 
 # Create your models here.
@@ -5,7 +6,9 @@ from django.db import models
 class Post(models.Model):
   title = models.CharField(max_length=30)
   content = models.TextField()
-
+  
+  head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+  file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   
